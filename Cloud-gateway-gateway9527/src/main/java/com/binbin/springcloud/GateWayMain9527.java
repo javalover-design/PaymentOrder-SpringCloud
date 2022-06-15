23b2a91fae25022ad13d69b6ac63d43bf53fe8cf
javalover-design <2279818505@qq.com>
2022-06-15 23:17:42 +0800

    Add GateWay With Services

---
+++ Cloud-gateway-gateway9527/src/main/java/com/binbin/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package com.binbin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author binbin
+ * @date 2022年06月15日  下午10:41
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
