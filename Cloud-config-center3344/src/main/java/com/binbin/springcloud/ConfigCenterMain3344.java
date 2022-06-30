4e03c5311e42b816412e99124b046dac94b8457c
javalover-design <2279818505@qq.com>
2022-06-30 23:59:12 +0800

    ConfigServer

--- Cloud-config-center3344/src/main/java/com/binbin/springcloud/ConfigCenterMain3344.java
+++ Cloud-config-center3344/src/main/java/com/binbin/springcloud/ConfigCenterMain3344.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author binbin
@@ -12,6 +13,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344.class,args);
