da9215475ab0d3e5e98e5d7e287fc91377ee61bd
javalover-design <2279818505@qq.com>
2022-07-02 02:03:09 +0800

    Configure Config-client

--- Cloud-config-client3355/src/main/java/com/binbin/springcloud/controller/ConfigClientController.java
+++ Cloud-config-client3355/src/main/java/com/binbin/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.binbin.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2022年07月01日  上午12:12
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
